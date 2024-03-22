63eb60a78c12014ead9eaef3ffe095605c0692be
abhis12321 <abhisheksingh2359328@gmail.com>
2024-03-23 01:07:42 +0530

    removing unneccessary files

--- src/app/news/[newsTag]/loading.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Loader() {
-    return (
-        <div  className="load-center">
-            <h1>Loading Data...</h1>
-        </div>
-    )
-}
