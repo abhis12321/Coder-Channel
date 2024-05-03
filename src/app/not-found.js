4333abc70545b87af6a0ad997703dd138e1d23bb
abhis12321 <abhisheksingh2359328@gmail.com>
2024-05-03 16:27:39 +0530

    custom 404 page added

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Not_found() {
+  return (
+    <div className='flex items-center justify-center' style={{minHeight:"calc(100vh - 4rem)"}}>
+      <h1 className="text-center mx-auto bg-inherit">4O4:Page Not Found</h1>
+    </div>
+  )
+}
