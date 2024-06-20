821bab9a8b427b4afcd72e08bf6af02265aacd63
abhis12321 <abhisheksingh2359328@gmail.com>
2024-06-20 06:16:41 +0530

    get blogs API is merged in frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,24 @@
 "use client"
-import React, { useState } from 'react'
-import PostForm from './_components/PostForm'
+import React, { useEffect, useState } from 'react'
+import BlogPost from './_components/BlogPost'
+import axios from 'axios';
 
 export default function Page() {
   const [postForm , setPostForm] = useState(false);
+  const [blogs , setBlogs] = useState();
+
+  useEffect(() => {
+    axios.get('/api/blogs')
+        .then(response => response.data)
+        .then(data => data.success && setBlogs(data.blogs))
+        .catch(error => console.log(error.message));
+  } , []);
 
   return (
     <div className='flex flex-col gap-3 items-center justify-center font-semibold' style={{minHeight:"calc(100vh - 4rem"}}>
       {
         postForm ? 
-        <PostForm setPostForm={setPostForm}/>
+        <BlogPost setPostForm={setPostForm}/>
         :
         <h1 className="py-2 px-6 text-red-950 hover:text-white dark:text-blue-600 dark:hover:text-white bg-red-950/20 hover:bg-red-950 dark:bg-blue-600/20 dark:hover:bg-blue-600/50 ring-1 ring-red-950 dark:ring-blue-600 dark:hover:ring-white rounded-xl cursor-pointer outline-none" onClick={e => setPostForm(true)}>write a new post/blog</h1>      
       }
