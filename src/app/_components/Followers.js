913595bd9230574ffa5da71528e01c90afa32206
abhis12321 <abhisheksingh2359328@gmail.com>
2024-06-28 11:47:54 +0530

    Followers and Followings components' styling modified(position:fixed)

--- src/app/_components/Followers.js
+++ src/app/_components/Followers.js
@@ -1,18 +1,20 @@
 import Link from 'next/link';
 import React from 'react'
 
-export default function Followers({followers , setConnections}) {
+export default function Followers({ followers, setConnections }) {
   return (
-    <div className='flex gap-[1px] flex-col items-center justify-around w-[98%] mx-[2%] max-w-[400px] bg-white dark:bg-blue-950 p-2 absolute z-10 top-24 ring-1 rounded'>
-      <div className="flex items-center justify-between w-full px-2 py-[6px]">
-        <div className="">Followers : {followers.length}</div>
-        <div className="text-xl text-red-800 font-semibold px-[10px] py-[2px] cursor-pointer hover:text-red-500 hover:ring-red-700 rounded-full ring-1 duration-500" onClick={e => setConnections(0)}>X</div>
-      </div>
-      {
-        followers?.map((user , index) => 
+    <div className="fixed z-10 top-0 left-0 w-[100%] h-[100%] bg-blue-100/10 flex items-center justify-center">
+      <div className='ring-1 flex gap-[1px] flex-col items-center justify-around w-[98%] mx-[2%] max-w-[400px] bg-white dark:bg-blue-950 p-2 rounded'>
+        <div className="flex items-center justify-between w-full px-2 py-[6px]">
+          <div className="">Followers : {followers.length}</div>
+          <div className="text-xl text-red-800 font-semibold px-[10px] py-[2px] cursor-pointer hover:text-red-500 hover:ring-red-700 rounded-full ring-1 duration-500" onClick={e => setConnections(0)}>X</div>
+        </div>
+        {
+          followers?.map((user, index) =>
             <Link href={`/students/${user.followedById}`} className="flex justify-between items-center p-2 font-mono bg-blue-400/20 rounded w-full hover:bg-green-400/20 text-red-950 dark:text-white" key={index}>{user.followedByName}</Link>
-        ) 
-      }
+          )
+        }
+      </div>
     </div>
   )
 }
--- src/app/_components/Followings.js
+++ src/app/_components/Followings.js
@@ -1,24 +1,26 @@
 import Link from 'next/link';
 import React from 'react'
 
-export default function Followings({followings , setConnections}) {
+export default function Followings({ followings, setConnections }) {
   // const handleUnFollow = () => {
   // }
 
   return (
-    <div className='flex gap-[1px] flex-col items-center justify-around w-[98%] mx-[2%] max-w-[400px] bg-white dark:bg-blue-950 p-2 absolute z-10 top-24 ring-1 rounded'>
-      <div className="flex items-center justify-between w-full px-2 py-[6px]">
-        <div className="">Followings : {followings.length}</div>
-        <div className="text-xl text-red-800 font-semibold px-[10px] py-[2px] cursor-pointer rounded-full ring-1 hover:ring-red-600 hover:text-red-500 duration-500" onClick={e => setConnections(0)}>X</div>
+    <div className="fixed z-10 top-0 left-0 w-[100%] h-[100%] bg-blue-100/10 flex items-center justify-center">
+      <div className='flex gap-[1px] flex-col items-center justify-around w-[98%] mx-[2%] max-w-[400px] bg-white dark:bg-blue-950 p-2 ring-1 rounded'>
+        <div className="flex items-center justify-between w-full px-2 py-[6px]">
+          <div className="">Followings : {followings.length}</div>
+          <div className="text-xl text-red-800 font-semibold px-[10px] py-[2px] cursor-pointer rounded-full ring-1 hover:ring-red-600 hover:text-red-500 duration-500" onClick={e => setConnections(0)}>X</div>
+        </div>
+        {
+          followings?.map((user, index) =>
+            <div className="flex justify-between items-center p-2 font-mono bg-blue-400/20 rounded w-full hover:bg-green-400/20 text-red-950 dark:text-white" key={index}>
+              <Link href={`/students/${user.followedToId}`} className="flex-1 font-bold">{user.followedToName}</Link>
+              {/* <button className="py-1 w-[70px] text-center rounded bg-red-950/50 hover:bg-red-600" onClick={handleUnFollow}>unfollow</button> */}
+            </div>
+          )
+        }
       </div>
-      {
-        followings?.map((user , index) => 
-          <div className="flex justify-between items-center p-2 font-mono bg-blue-400/20 rounded w-full hover:bg-green-400/20 text-red-950 dark:text-white"  key={index}>
-            <Link href={`/students/${user.followedToId}`} className="flex-1 font-bold">{user.followedToName}</Link>
-            {/* <button className="py-1 w-[70px] text-center rounded bg-red-950/50 hover:bg-red-600" onClick={handleUnFollow}>unfollow</button> */}
-          </div>
-        ) 
-      }
     </div>
   )
 }
