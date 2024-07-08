e289fef955153150a0fd5e0bcc7e7b069f5d996c
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-08 12:58:37 +0530

    some styling changed

--- src/app/_components/LoginForm.js
+++ src/app/_components/LoginForm.js
@@ -1,39 +1,39 @@
 import Link from 'next/link'
 import React from 'react'
-import {useAuth} from '../_components/AuthProvider';
+import { useAuth } from '../_components/AuthProvider';
 
 export default function LoginForm() {
-    const [password , setPass] = React.useState("");
-    const [email , setEmail] = React.useState("");  
-    let USER = useAuth();
-    
-  const handleLogin = async(e) => {
+  const [password, setPass] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  let USER = useAuth();
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-    USER.login({email , password});
+    USER.login({ email, password });
   }
 
   return (
     <div className="flex flex-col items-center gap-3 w-full">
-    <h2 className="font-extrabold text-4xl text-center rounded-md text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-violet-600 w-fit pb-2 leading-12 drop-shadow-[0_0_1px_black]">Login Page</h2>
-    <form onSubmit={handleLogin} className='from-blue-50 to-blue-50 bg-gradient-to-b  dark:from-blue-900/50 dark:via-cyan-950/90 dark:to-blue-950/60 shadow-[0_0_4px_black] dark:shadow-[0_0_4px_white] w-[98%] max-w-[500px] py-4 px-5 rounded-2xl min-h-[280px] text-white flex flex-col gap-3 items-center justify-around' autoComplete='on'>
-      
-      <div className="bgslate-950/20 py-2 px-10 rounded-lg">
-        {/* <p className="hidden sm:flex text-center text-sm font-mono text-yellow-400 font-semibold drop-shadow-[0_0_7px_white]">Login with your email-id</p> */}
-      </div>
+      <h2 className="font-extrabold text-4xl text-center rounded-md text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-violet-600 w-fit pb-2 leading-12 drop-shadow-[0_0_1px_black]">Login Page</h2>
+      <form onSubmit={handleLogin} className='from-blue-50 to-blue-50 bg-gradient-to-b  dark:from-blue-900/50 dark:via-cyan-950/90 dark:to-blue-950/60 shadow-[0_0_10px_black] dark:shadow-[0_0_4px_white] w-[98%] max-w-[500px] py-4 px-5 rounded-2xl min-h-[280px] text-white flex flex-col gap-3 items-center justify-around' autoComplete='on'>
 
-      <input type="email" value = {email} onChange={(e)=> setEmail(e.target.value)} className='bg-blue-950/10 dark:bg-red-600/15 px-2 py-[10px] rounded text-center w-[96%] max-w-[550px] outline-none focus:bg-blue-600/20 ring-1 focus:ring-2 ring-violet-700 dark:ring-gray-400' placeholder='email' name='email' required/>
-      
-      <input type="password" value = {password} onChange={(e)=> setPass(e.target.value)} className='bg-blue-950/10 dark:bg-red-600/15 px-2 py-[10px] rounded text-center w-[96%] max-w-[550px] outline-none focus:bg-blue-600/20 ring-1 focus:ring-2 ring-violet-700 dark:ring-gray-400' placeholder='password' name='password' required/>
+        <div className="bgslate-950/20 py-1 px-10 rounded-lg">
+          {/* <p className="hidden sm:flex text-center text-sm font-mono text-yellow-400 font-semibold drop-shadow-[0_0_7px_white]">Login with your email-id</p> */}
+        </div>
 
-      <div className="flex items-center justify-evenly w-[98%] max-w-[550px] text-xs xs:text-sm" >
-        <Link href={`/login/resend`} className="text-blue-600 font-semibold underline hover:text-blue-700 hover:scale-110">Resend Verification Link</Link>
-        <Link href={`/login/forgot`} className="text-blue-600 font-semibold underline hover:text-blue-700 hover:scale-110">forgot password</Link>
-      </div>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className='bg-blue-950/10 dark:bg-red-600/15 px-2 py-[10px] rounded text-center w-[96%] max-w-[550px] outline-none focus:bg-green-600/15 ring-1 focus:ring-2 ring-violet-700 dark:ring-gray-400' placeholder='email' name='email' required />
 
-      <div className="w-[96%] max-w-[550px] flex justify-between">
-        <Link href={`/login/registration`} className='w-[120px] xs:w-[150px] text-center rounded-md bg-red-900/95 py-2 hover:bg-red-700 font-mono' >Resister New</Link>
-        <button type='submit' className='w-[120px] xs:w-[150px] text-center rounded-md bg-red-900/95 py-2 hover:bg-red-700 font-mono' >Login</button>
-      </div>
-    </form></div>
+        <input type="password" value={password} onChange={(e) => setPass(e.target.value)} className='bg-blue-950/10 dark:bg-red-600/15 px-2 py-[10px] rounded text-center w-[96%] max-w-[550px] outline-none focus:bg-green-600/15 ring-1 focus:ring-2 ring-violet-700 dark:ring-gray-400' placeholder='password' name='password' required />
+
+        <div className="flex items-center justify-evenly w-[98%] max-w-[550px] text-xs xs:text-sm" >
+          <Link href={`/login/resend`} className="text-blue-600 font-semibold underline hover:text-blue-700 hover:scale-110">Resend Verification Link</Link>
+          <Link href={`/login/forgot`} className="text-blue-600 font-semibold underline hover:text-blue-700 hover:scale-110">forgot password</Link>
+        </div>
+
+        <div className="w-[96%] max-w-[550px] flex justify-between">
+          <Link href={`/login/registration`} className='w-[120px] xs:w-[150px] text-center rounded-md bg-red-900/95 py-2 hover:bg-red-700 font-mono' >Resister New</Link>
+          <button type='submit' className='w-[120px] xs:w-[150px] text-center rounded-md bg-red-900/95 py-2 hover:bg-red-700 font-mono' >Login</button>
+        </div>
+      </form></div>
   )
 }
--- src/app/_components/ToggleMode.js
+++ src/app/_components/ToggleMode.js
@@ -9,9 +9,9 @@ export default function ToggleMode() {
         document.body.classList.toggle('dark');
     }
   return (
-    <div className='group cursor-pointer *:rounded-full flex items-center justify-center h-8 w-8 mx-auto p-6 lg:p-[22px] my-2 lg:my-0 rounded-full lg:ml-2 md:hover:scale-110 hover:ring-2 ring-gray-200  hover:bg-slate-950/30 dark:hover:bg-gray-200/15' onClick={toggleMode}>
-      <FontAwesomeIcon icon={faSun} className='p-2 bg-gray-950 shadow-[0_0_1px_white] text-lg text-white ring-1 ring-gray-200 group-hover:ring-0' id='dark'/>
-      <FontAwesomeIcon icon={faMoon} className='text-slate-700 bg-gray-200 py-2 px-[10px] hidden' id='light'/>
+    <div className='group cursor-pointer *:rounded-full flex items-center justify-center h-8 w-8 mx-auto p-6 lg:p-[24px] my-2 lg:my-0 rounded-full lg:ml-2 hover:ring-2 ring-gray-200  hover:bg-slate-950/30 dark:hover:bg-gray-200/15' onClick={toggleMode}>
+      <FontAwesomeIcon icon={faSun} className='p-2 shadow-[0_0_1px_white] text-2xl text-white ring-1 ring-gray-200 group-hover:ring-0' id='dark'/>
+      <FontAwesomeIcon icon={faMoon} className='text-white text-[1.67rem] py-[7px] px-[10px] hidden ring-1 group-hover:ring-0 ring-white' id='light'/>
     </div>
   )
 }
