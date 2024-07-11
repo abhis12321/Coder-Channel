d0571fd47ad88ec07bca00df1ddf1cb05465d9cc
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-11 19:41:11 +0530

    chat-model is created for storing the chats in database

---
+++ mongo/ChatModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import dbConnect from './dbConnect'
+
+dbConnect();
+const chatSchema = new mongoose.Schema({
+    senderId:{
+        type:String,
+        required:[true , "sender-id is missing."],
+    },
+    senderName:{
+        type:String,
+        required:[true , "sender-name is missing."],
+    },
+    receiverId:{
+        type:String,
+        required:[true , "sender-id is missing."],
+    },
+    receiverName:{
+        type:String,
+        required:[true , "sender-name is missing."],
+    },
+});
+
+
+export const Chat = mongoose.models.Chat || mongoose.model("Chat" , chatSchema);
