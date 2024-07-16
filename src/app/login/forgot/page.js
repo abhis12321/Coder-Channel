28b6e2d5d401f00b3c7b5f3e6a2670c842592f19
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-16 23:28:12 +0530

    AWS deployement related changes

--- src/app/login/forgot/page.js
+++ src/app/login/forgot/page.js
@@ -23,7 +23,7 @@ export default function Page() {
             <form className="bg-gradient-to-br from-white to-white dark:from-gray-900 dark:via-cyan-950 dark:to-slate-950 px-2 py-8 flex flex-col gap-3 items-center justify-center w-[98%] max-w-[650px] rounded-xl text-white shadow-[0_0_10px_gray]" onSubmit={handleSubmit} autoComplete="on">
                 <h1 className="mx-auto text-xl font-bold text-center w-fit text-yellow-600 font-mono">send password to email</h1>
                 <input type="email" name = 'email'  className="bg-blue-700/15 dark:bg-slate-950/50 w-[95%] font-semibold shadow-[0_0_3px_white] py-[10px] px-4 text-sm rounded-lg text-center outline-none focus:ring-blue-600 ring-1 text-red-950 dark:text-white" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="enter your email" required/>
-                <button type='submit' className='w-[95%] font-semibold shadow-[0_0_3px_white] bg-red-900/80 hover:bg-red-700 active:bg-violet-900 py-2 focus:bg-cyan-950 px-4 rounded-xl mx-auto text-center' >send</button>
+                <button type='submit' className='w-[95%] font-semibold shadow-[0_0_3px_white] bg-red-900/80 hover:bg-red-700 active:bg-violet-900 py-2 px-4 rounded-xl mx-auto text-center' >send</button>
             </form>
             :
             <h1 className="mx-auto py-4 px-8 rounded-xl text-xl font-bold bg-cyan-950 text-center w-fit shadow-[0_0_5px_white] text-yellow-600">You are already logged-in</h1>
