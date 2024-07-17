30b8433d69b09e368aab338a679119066b59975f
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-17 13:41:04 +0530

    AWS deployment related changes - 7 and added stylings

--- src/app/login/[token]/page.js
+++ src/app/login/[token]/page.js
@@ -26,8 +26,8 @@ export default function Page({params , searchParams}) {
   return (
     <div className="flex items-center justify-center h-nav">
       {!USER.user ?
-            <form className="bg-gradient-to-tr from-gray-900 via-cyan-950 to-slate-950 py-12 flex flex-col gap-4 items-center justify-center w-[95%] max-w-[800px] rounded-xl text-white shadow-[0_0_12px_gray]" onSubmit={handleSubmit} autoComplete="on">
-                <h1 className=" mx-auto py-4 px-8 rounded-xl text-xl font-bold bg-slate-950/10 text-center w-fit shadow-[0_0_5px_white] text-yellow-600">Verify Your Email</h1>
+            <form className="from-blue-50 to-blue-50 bg-gradient-to-b  dark:from-blue-900 dark:via-cyan-950/90 dark:to-blue-950 shadow-[0_0_10px_black] dark:shadow-[0_0_4px_white] px-2 py-8 flex flex-col gap-3 items-center justify-center w-[98%] max-w-[650px] rounded-xl" onSubmit={handleSubmit} autoComplete="on">
+                <h1 className=" mx-auto rounded-xl text-xl font-bold text-center w-fit text-yellow-600">Verify Your Email</h1>
                 <input type="password" name = 'email'  className="bg-slate-950/50 w-[95%] xs:w-[80%] font-semibold shadow-[0_0_3px_white] py-2 px-4 text-md rounded-lg text-center outline-none" value={pass} onChange={(e) => setPass(e.target.value)} placeholder="enter password" required/>
                 <button type='submit' className='w-[95%] xs:w-[80%] font-semibold shadow-[0_0_3px_white] bg-red-900/80 hover:bg-red-700 active:bg-violet-900 py-2 focus:bg-cyan-950 px-4 rounded-xl mx-auto text-center' >verify</button>
             </form>
