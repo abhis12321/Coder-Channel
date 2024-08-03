68607628db6a4ed9e51711288866781b74084a97
abhis12321 <abhisheksingh2359328@gmail.com>
2024-08-03 17:06:28 +0530

    PUT API is created for storing theme info into cache and feching back from there

--- src/app/_components/ToggleMode.js
+++ src/app/_components/ToggleMode.js
@@ -1,17 +1,24 @@
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useAuth } from './AuthProvider';
+import axios from 'axios';
 
 
 export default function ToggleMode() {
   const { theme , setTheme } = useAuth();
     const toggleMode = e => {
+      axios.put("/api" , { theme:theme === "dark" ? "" : "dark" })
+          .catch(error => console.log(error.message));
       setTheme(prev => prev === "dark" ? "" : "dark");
     }
   return (
     <div className='group cursor-pointer *:rounded-full flex items-center justify-center h-8 w-8 mx-auto p-6 lg:p-[23px] my-2 lg:my-0 rounded-full lg:ml-2 hover:ring-2 ring-gray-200  hover:bg-slate-950/30 dark:hover:bg-gray-200/15' onClick={toggleMode}>
-      <FontAwesomeIcon icon={faSun} className='p-2 shadow-[0_0_1px_white] text-2xl text-white ring-1 ring-gray-200 group-hover:ring-0' id='dark'/>
-      <FontAwesomeIcon icon={faMoon} className='text-white text-[1.6rem] bg-slate-950/30 py-[6px] px-[9px] hidden ring-1 group-hover:ring-0 ring-gray-400' id='light'/>
+      {
+        theme === "dark" ?
+        <FontAwesomeIcon icon={faSun} className='p-2 shadow-[0_0_1px_white] text-2xl text-white ring-1 ring-gray-200 group-hover:ring-0' id='dark'/>
+        :
+      <FontAwesomeIcon icon={faMoon} className='text-white bg-gray-950/20 text-[1.65rem] py-[6px] px-[9px] shadow-[0_0_2px_gray] group-hover:shadow-none' id='light'/>
+      }
     </div>
   )
 }
--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import User from "/mongo/UserModel";
 import mongoose from "mongoose";
+import { cookies } from 'next/headers';
 
 export async function GET() {
   try {
@@ -57,5 +58,15 @@ export async function POST(req) {
     return NextResponse.json({ success: true, users });
   } catch (error) {
     return NextResponse.json({ success: false, message: error.message });
+  }
+}
+
+export async function PUT(req) {
+  try {
+      let { theme } = await req.json();
+      cookies().set("theme" , theme);
+      return NextResponse.json({ success:true , message:"theme updated." });
+  } catch(error) {
+      return NextResponse.json({ success:false , message:error.message });
   }
 }
