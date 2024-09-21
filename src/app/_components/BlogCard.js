07bac59a8631f85bb73950ad17422be0e5f3afaf
abhis12321 <abhisheksingh2359328@gmail.com>
2024-09-22 03:59:02 +0530

    new component is added and the UI of the BlogCard is modified

--- src/app/_components/BlogCard.js
+++ src/app/_components/BlogCard.js
@@ -1,18 +1,30 @@
 import { faPenClip } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import BlogFooter from './BlogFooter';
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image';
 
-export default function Blogs( { blog } ) {
+export default function Blogs({ blog }) {
   return (
-    <div className='w-[98%] max-w-[700px] min-h-20 min-w-20 bg-white ring-gray-500 dark:bg-green-800/20 ring-1 dark:ring-green-800 rounded-lg pt-4 pb-2 px-3 xs:px-4 md:px-5 flex flex-col gap-2  justify-evenly items-center'>
-      <div className="w-full font-mono font-medium text-gray-700 dark:text-white whitespace-pre-wrap">
+    <div className='w-[98%] max-w-[700px] min-h-20 min-w-20 ring-1 ring-gray-500 dark:ring-green-800 bg-white dark:bg-green-800/10 dark:hover:scale-[1.004] rounded-lg pt-2 pb-1 px-3 xs:px-4 md:px-5 flex flex-col gap-1 justify-evenly items-center'>
+      <Link href={`/students/${blog.writerId}`} className="w-full h-16 px-4 py-5 rounded-lg flex gap-5 items-center hover:bg-blue-600/20 hover:animate-pulse">
+        <Image src={blog.userImage || "/img/profileImg.jpg"} alt='' height={50} width={50} className='rounded-full h-[55px] w-[55px]' />
+        <div className="flex flex-col justify-around">
+          <div className="text-lg">{blog.writer}</div>
+          <div className="text-red-950/60 dark:text-gray-500/85 text-xs">{`${(new Date(blog.time)).toDateString()} ${(new Date(blog.time)).toLocaleTimeString()}`}</div>
+        </div>
+      </Link>
+      <div className='h-[1px] w-full mb-4 bg-gray-600/50' />
+      <div className="w-full font-mono font-medium text-gray-700 dark:text-gray-300/90 whitespace-pre-wrap">
         {blog.blog}
       </div>
-      <div className="text-[11px] flex gap-2 self-end font-serif">
+      {/* <div className="text-[11px] flex gap-2 self-end font-serif">
         <Link href={`/students/${blog.writerId}`} className="underline hover:scale-110 font-extrabold text-blue-800 hover:text-blue-600">{blog.writer} <FontAwesomeIcon size='sm' icon={faPenClip} className='h-[10px]' /></Link>
         <div className="text-red-950/60 dark:text-gray-500/85 font-bold">{`${(new Date(blog.time)).toDateString()} ${(new Date(blog.time)).toLocaleTimeString()}`}</div>
-      </div>
+      </div> */}
+      <div className='h-[1px] w-full mt-4 bg-gray-600/50' />
+      <BlogFooter />
     </div>
   )
 }
--- src/app/_components/ToggleMode.js
+++ src/app/_components/ToggleMode.js
@@ -1,7 +1,7 @@
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useAuth } from './AuthProvider';
-import axios from 'axios';
+import { useAuth } from './AuthProvider'
+import axios from 'axios'
 
 
 export default function ToggleMode() {
@@ -19,12 +19,12 @@ export default function ToggleMode() {
   }
 
   return (
-    <form className="w-[47px] aspect-square mx-auto lg:mx-4 mt-4 lg:my-0 rounded-full cursor-pointer bg-white dark:bg-inherit hover:bg-gray-500 dark:hover:bg-gray-400/30 flex items-center justify-center shadow-[0_0_2px_gray_inset] overflow-hidden text-gray-500 dark:text-white hover:text-white hover:scale-110 duration-500" onSubmit={handleToggleMode}>
-      <button className={`h-full w-full text-[1.5rem] ${!themeRef?.current?.classList?.contains("dark") && "hidden"}`} name='dark'>
-        <FontAwesomeIcon icon={faSun} className='h-[23px] ' />
+    <form className="w-[47px] aspect-square mx-auto lg:mx-4 mt-4 lg:my-0 rounded-full cursor-pointer bg-white dark:bg-inherit hover:bg-gray-500 dark:hover:bg-gray-400/30 flex items-center justify-center shadow-[0_0_2px_gray_inset] overflow-hidden text-gray-500 dark:text-white hover:text-white hover:scale-110 duration-500 hover:ring-1 ring-gray-500 dark:ring-white" onSubmit={handleToggleMode}>
+      <button className={`w-full h-full flex items-center justify-center ${!themeRef?.current?.classList?.contains("dark") && "hidden"}`} name='dark'>
+        <FontAwesomeIcon icon={faSun} className='h-[23px]' />
       </button>
-      <button className={`h-full w-full text-[1.7rem] ${themeRef?.current?.classList?.contains("dark") && "hidden"}`} name='light'>
-        <FontAwesomeIcon icon={faMoon} className='h-[25px] ' />
+      <button className={`w-full h-full flex items-center justify-center ${themeRef?.current?.classList?.contains("dark") && "hidden"}`} name='light'>
+        <FontAwesomeIcon icon={faMoon} className='h-[25px]' />
       </button>
     </form>
   )
