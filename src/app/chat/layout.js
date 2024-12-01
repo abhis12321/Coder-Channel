72f9ea64dda50d0b1b5ec5b643bf7064bf213525
abhis12321 <abhisheksingh2359328@gmail.com>
2024-12-01 11:44:16 +0530

    modified the UI of the messagensers list UI and some unctions

--- src/app/chat/layout.js
+++ src/app/chat/layout.js
@@ -31,7 +31,7 @@ export default function layout({ children }) {
         listRef?.current.classList.toggle("-left-full");
         listRef?.current.classList.toggle("left-0");
         // menuRef?.current.classList.toggle("flex");
-        // menuRef?.current.classList.toggle("hidden");
+        menuRef?.current.classList.toggle("hidden");
     }
 
     const hideUsersList = () => {
@@ -47,21 +47,21 @@ export default function layout({ children }) {
                     <LoginForm />
                     :
                     <div className="w-full h-full flex justify-center relative bg-red400">
-                        <div className="max-h-nav w-[40%] h-[100%] md:h-auto max-w-[300px] absolute z-10 md:static top-0 -left-full flex flex-col gap-[1px] bg-blue-50 dark:bg-gray-800/90 dark:text-white shadow-[-1px_-.5px_1px_gray_inset] dark:shadow-[-1px_-.5px_1px_white_inset] duration-500 backdrop-blur-sm md:backdrop-blur-none" ref={listRef}>
-                            <Link href="/chat" className='w-full py-[8px] px-1 md:px-2 rounded flex gap-2 items-center bg-gray-600/20 hover:bg-violet-700/40 shadow-[0_0_1px_gray_inset] group overflow-y-auto overflow-x-hidden new-scroll'>
+                        <div className="max-h-nav w-[40%] h-[100%] md:h-auto max-w-[300px] absolute z-10 md:static top-0 -left-full flex flex-col gap-[1px] bg-blue-50 dark:bg-blue-800/30 dark:text-white shadow-[-1px_-.5px_1px_gray_inset] dark:shadow-[-1px_-.5px_1px_white_inset] duration-500 backdrop-blur-md md:backdrop-blur-none new-scroll overflow-x-hidden" ref={listRef}>
+                            <Link href="/chat" className='w-full py-[9px] px-1 md:px-2 rounded flex gap-2 items-center bg-gray-600/20 dark:bg-black/20 hover:bg-violet-700/40 shadow-[0_0_1px_gray_inset] group'>
                                 <FontAwesomeIcon size='sm' icon={faUsersLine} className='h-8'  />
-                                <span className="flex flex-col group-hover:underline font-bold text-lg">Public Group</span>
+                                <span className="flex flex-col group-hover:underline font-bold text-lg text-nowrap">Public Group</span>
                             </Link>
-                            {senders?.map(sender => <Link href={`/chat/${sender?._id}`} key={sender?._id} className='w-full py-[2px] px-1 md:px-2 rounded flex gap-2 items-center bg-gray-600/20 hover:bg-cyan-700/40 dark:hover:bg-cyan-600/15 shadow-[0_0_1px_gray_inset] group'>
+                            {senders?.map(sender => <Link href={`/chat/${sender?._id}`} key={sender?._id} className='w-full py-[2px] px-1 md:px-2 rounded flex gap-2 items-center bg-gray-600/20 dark:bg-black/20 hover:bg-cyan-700/50 dark:hover:bg-cyan-600/15 shadow-[0_0_1px_gray_inset] hover:shadow-red-700 hover:shadow-sm group'>
                                 <Image src={sender?.imgUrl} alt='' height={50} width={50} className='w-10 aspect-square rounded-full' />
                                 <span className="flex flex-col">
-                                    <span className="font-semibold group-hover:underline">{sender?.name}</span>
-                                    <span className="text-xs font-light">@{sender?.university}</span>
+                                    <span className="font-semibold group-hover:underline text-nowrap">{sender?.name}</span>
+                                    <span className="text-xs font-light text-nowrap">@{sender?.university}</span>
                                 </span>
                             </Link>)}
                         </div>
 
-                        <button className="flex md:hidden absolute top-5 left-2 z-10 font-bold active:text-violet-700" onClick={handleUsersList} onBlur={hideUsersList} ref={menuRef}><FontAwesomeIcon size='sm' icon={faCommentDots} className='h-7' /></button>
+                        <button className="flex md:hidden absolute top-6 left-4 z-10 font-bold active:text-orange-600 drop-shadow-[0_0_2px_black] bg-green-600 rounded-3xl px-[1px]" onClick={handleUsersList} onBlur={hideUsersList}><FontAwesomeIcon size='sm' icon={faCommentDots} className='h-7' ref={menuRef} /></button>
                         {
                             children
                         }
